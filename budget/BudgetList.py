4d4511f95579e896afc87fbe90b0338dfb8569f1
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 01:23:47 -0500

    added str()

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -25,7 +25,7 @@ class BudgetList:
         expenses.read_expenses("data/spending_data.csv")
         for expense in expenses.list:
             myBudgetList.append(expense.amount)
-        print('The count of all expenses: ' + len(myBudgetList))
+        print('The count of all expenses: ' + str(len(myBudgetList)))
     
     if __name__ == "__main__":
         main()
