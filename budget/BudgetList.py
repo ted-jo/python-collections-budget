9a842370db8a4f962e8cf239228bc0306c135e21
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 01:28:44 -0500

    Update

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,6 @@
 from . import Expense
 
-class BudgetList:
+class BudgetList():
     def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
@@ -8,25 +8,27 @@ class BudgetList:
         self.sum_overages = 0
         self.overages = []
 
+    def __len__(self):
+        return (len(self.expenses) + len(self.overages))
+
     def append(self, item):
-        if self.sum_expenses + item < self.budget:
+        if (self.sum_expenses+item < self.budget):
             self.expenses.append(item)
             self.sum_expenses += item
         else:
             self.overages.append(item)
-            self.sum_overages += item
+            self.sum_overages+=item
 
-    def __len__(self):
-        return len(self.expenses) + len(self.overages)
 
-    def main():
-        myBudgetList = BudgetList(1200)
-        expenses = Expense.Expenses()
-        expenses.read_expenses("data/spending_data.csv")
-        for expense in expenses.list:
-            myBudgetList.append(expense.amount)
-        print('The count of all expenses: ' + str(len(myBudgetList)))
-    
-    if __name__ == "__main__":
-        main()
 
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+
+if __name__ == "__main__":
+    main() 
