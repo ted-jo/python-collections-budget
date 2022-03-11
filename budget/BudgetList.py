7a7b6689b3d9020f94336bc1247f2342fb80ccf2
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 01:43:24 -0500

    Fixed issues

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,5 @@
-from tracemalloc import stop
-from . import Expense
 import matplotlib.pyplot as plt
+from . import Expense
 
 class BudgetList():
     def __init__(self, budget):
@@ -21,6 +20,16 @@ class BudgetList():
             self.overages.append(item)
             self.sum_overages+=item
 
+    def __iter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e.__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
 
 
 def main():
@@ -32,24 +41,13 @@ def main():
     print('The count of all expenses: ' + str(len(myBudgetList)))
     for entry in myBudgetList:
         print(entry)
-    fig, ax = plt.subplots()
+
+    fig,ax=plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
-    ax.bar(labels, values, color=['green','red','blue'])
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
     ax.set_title('Your total expenses vs. total budget')
     plt.show()
 
-
 if __name__ == "__main__":
     main() 
-
-def __iter__(self):
-    iter(self.expenses)
-    self.iter_o = iter(self.overages)
-    return self
-
-def __next__(self):
-    try:
-        return self.iter_e.__next__()
-    except StopIteration as stop:
-        return self.iter_o.__next__()
