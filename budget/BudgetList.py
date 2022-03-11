3e1da4a9eaf76f6c16b4d2aa2037e110c7758681
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 01:41:36 -0500

    Added Iters and main graphing functions

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,6 @@
+from tracemalloc import stop
 from . import Expense
+import matplotlib.pyplot as plt
 
 class BudgetList():
     def __init__(self, budget):
@@ -28,7 +30,26 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
     print('The count of all expenses: ' + str(len(myBudgetList)))
+    for entry in myBudgetList:
+        print(entry)
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green','red','blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
 
 if __name__ == "__main__":
     main() 
+
+def __iter__(self):
+    iter(self.expenses)
+    self.iter_o = iter(self.overages)
+    return self
+
+def __next__(self):
+    try:
+        return self.iter_e.__next__()
+    except StopIteration as stop:
+        return self.iter_o.__next__()
